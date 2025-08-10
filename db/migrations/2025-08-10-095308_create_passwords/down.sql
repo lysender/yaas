8a91e696face66a5746fb9b8b5089e057da90f75
Lysender <lysender@darkstar-win>
2025-08-10 17:56:09 +0800

    fix: add passwords table

---
+++ db/migrations/2025-08-10-095308_create_passwords/down.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS trigger_update_updated_at ON passwords;
+
+DROP TABLE IF EXISTS passwords;
--- db/src/schema.rs
+++ db/src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    passwords (id) {
+        #[max_length = 36]
+        id -> Bpchar,
+        #[max_length = 250]
+        password -> Varchar,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
     users (id) {
         #[max_length = 36]
         id -> Bpchar,
@@ -14,3 +25,8 @@ diesel::table! {
         updated_at -> Timestamptz,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    passwords,
+    users,
+);
