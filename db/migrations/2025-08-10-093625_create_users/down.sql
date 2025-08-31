9e7b377e2f357ff21bb9e9327efe2c5096c82eeb
Lysender <lysender@darkstar-win>
2025-08-31 14:22:58 +0800

    fix: users.email/deleted_at composite unique key

--- db/migrations/2025-08-10-093625_create_users/down.sql
+++ db/migrations/2025-08-10-093625_create_users/down.sql
@@ -1,3 +1,4 @@
+DROP INDEX IF EXISTS idx_users_email_deleted_at;
 DROP INDEX IF EXISTS idx_users_deleted_at;
 
 DROP TABLE IF EXISTS users;
--- db/migrations/2025-08-10-093625_create_users/up.sql
+++ db/migrations/2025-08-10-093625_create_users/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id CHAR(36) PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL,
     name VARCHAR(100) NOT NULL,
     status VARCHAR(10) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -8,4 +8,5 @@ CREATE TABLE users (
     deleted_at TIMESTAMP WITH TIME ZONE NULL DEFAULT NULL
 );
 
+CREATE UNIQUE INDEX idx_users_email_deleted_at ON users(email, deleted_at);
 CREATE INDEX idx_users_deleted_at ON users(deleted_at);
