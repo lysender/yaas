68ba9fc6282312cf6dae0b5092e95cad5b6aed27
Lysender <lysender@darkstar-win>
2025-08-31 17:45:26 +0800

    fix: cleanup protogen

--- protogen/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod buffed {
-    pub mod yaas {
-        include!(concat!(env!("OUT_DIR"), "/buffed.rs"));
-    }
-}
-
-use buffed::yaas::UserDto;
---
+++ protogen/src/main.rs
@@ -0,0 +1,5 @@
+use yaas::buffed::OrgBuf;
+
+fn main() {
+    println!("Hello, world!");
+}
