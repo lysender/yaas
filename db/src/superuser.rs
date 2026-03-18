70e421d6050c5269307a30801f20209c7186138b
Lysender <lysender@darkstar>
2026-03-18 20:13:51 +0800

    fix: migrate superuser repo

--- db/src/superuser.rs
+++ db/src/superuser.rs
@@ -1,31 +1,19 @@
-use chrono::{DateTime, SecondsFormat, Utc};
-use deadpool_diesel::postgres::Pool;
-use diesel::prelude::*;
-use diesel::result::Error;
-use diesel::{QueryDsl, SelectableHelper};
 use snafu::ResultExt;
-use turso::Connection;
+use turso::{Connection, Row};
 
 use crate::Result;
-use crate::error::{DbInteractSnafu, DbPoolSnafu, DbQuerySnafu};
-use crate::schema::org_members;
-use crate::schema::orgs;
-use crate::schema::passwords;
-use crate::schema::superusers::{self, dsl};
-use crate::schema::users;
+use crate::error::{DbPrepareSnafu, DbStatementSnafu, DbTransactionSnafu};
+use crate::turso_decode::{FromTursoRow, collect_row, collect_rows, row_integer, row_text};
+use crate::turso_params::{integer_param, new_query_params, text_param};
 use yaas::dto::{NewPasswordDto, NewUserDto, SuperuserDto};
+use yaas::utils::generate_id;
 
-pub struct Superuser {
-    pub id: String,
-    pub created_at: i64,
-}
-
-impl From<Superuser> for SuperuserDto {
-    fn from(user: Superuser) -> Self {
-        SuperuserDto {
-            id: user.id,
-            created_at: user.created_at,
-        }
+impl FromTursoRow for SuperuserDto {
+    fn from_row(row: &Row) -> Result<Self> {
+        Ok(Self {
+            id: row_text(row, 0)?,
+            created_at: row_integer(row, 1)?,
+        })
     }
 }
 
@@ -39,154 +27,263 @@ impl SuperuserRepo {
     }
 
     pub async fn setup(
-        &self,
+        &mut self,
         new_user: NewUserDto,
         new_password: NewPasswordDto,
     ) -> Result<SuperuserDto> {
-        let db = self.db_pool.get().await.context(DbPoolSnafu)?;
+        let user_id = generate_id("usr");
+        let org_id = generate_id("org");
+        let org_member_id = generate_id("omm");
+        let created_at = chrono::Utc::now().timestamp_millis();
 
-        let new_user_copy = new_user.clone();
+        let user_query = r#"
+            INSERT INTO users
+            (
+                id,
+                email,
+                name,
+                status,
+                created_at,
+                updated_at,
+                deleted_at
+            )
+            VALUES
+            (
+                :id,
+                :email,
+                :name,
+                :status,
+                :created_at,
+                :updated_at,
+                NULL
+            )
+        "#;
 
-        // Expects password to be already hashed
-        let new_password_copy = new_password.clone();
+        let mut user_params = new_query_params();
+        user_params.push(text_param(":id", user_id.clone()));
+        user_params.push(text_param(":email", new_user.email));
+        user_params.push(text_param(":name", new_user.name));
+        user_params.push(text_param(":status", "active".to_string()));
+        user_params.push(integer_param(":created_at", created_at));
+        user_params.push(integer_param(":updated_at", created_at));
 
-        let trans_res = db
-            .interact(move |conn| {
-                conn.transaction::<_, Error, _>(|conn| {
-                    let today = chrono::Utc::now();
+        let passwd_query = r#"
+            INSERT INTO passwords
+            (
+                id,
+                password,
+                created_at,
+                updated_at
+            )
+            VALUES
+            (
+                :id,
+                :password,
+                :created_at,
+                :updated_at
+            )
+        "#;
 
-                    // Create user
-                    let user_id = diesel::insert_into(users::table)
-                        .values((
-                            users::email.eq(new_user_copy.email),
-                            users::name.eq(new_user_copy.name),
-                            users::status.eq("active"),
-                            users::created_at.eq(today),
-                            users::updated_at.eq(today),
-                        ))
-                        .returning(users::id)
-                        .get_result::<i32>(conn)?;
+        let mut password_params = new_query_params();
+        password_params.push(text_param(":id", user_id.clone()));
+        password_params.push(text_param(":password", new_password.password));
+        password_params.push(integer_param(":created_at", created_at));
+        password_params.push(integer_param(":updated_at", created_at));
 
-                    // Create password
-                    let _ = diesel::insert_into(passwords::table)
-                        .values((
-                            passwords::id.eq(user_id),
-                            passwords::password.eq(new_password_copy.password),
-                            passwords::created_at.eq(today),
-                            passwords::updated_at.eq(today),
-                        ))
-                        .execute(conn)?;
+        let org_query = r#"
+            INSERT INTO orgs
+            (
+                id,
+                name,
+                status,
+                owner_id,
+                created_at,
+                updated_at,
+                deleted_at
+            )
+            VALUES
+            (
+                :id,
+                :name,
+                :status,
+                :owner_id,
+                :created_at,
+                :updated_at,
+                NULL
+            )
+        "#;
 
-                    // Create organization
-                    let org_id = diesel::insert_into(orgs::table)
-                        .values((
-                            orgs::name.eq("Superuser"),
-                            orgs::status.eq("active"),
-                            orgs::owner_id.eq(user_id),
-                            orgs::created_at.eq(today),
-                            orgs::updated_at.eq(today),
-                        ))
-                        .returning(orgs::id)
-                        .get_result::<i32>(conn)?;
+        let mut org_params = new_query_params();
+        org_params.push(text_param(":id", org_id.clone()));
+        org_params.push(text_param(":name", "Superuser".to_string()));
+        org_params.push(text_param(":status", "active".to_string()));
+        org_params.push(text_param(":owner_id", user_id.clone()));
+        org_params.push(integer_param(":created_at", created_at));
+        org_params.push(integer_param(":updated_at", created_at));
 
-                    // Add as member
-                    let _ = diesel::insert_into(org_members::table)
-                        .values((
-                            org_members::org_id.eq(org_id),
-                            org_members::user_id.eq(user_id),
-                            org_members::roles.eq("Superuser"),
-                            org_members::status.eq("active"),
-                            org_members::created_at.eq(today),
-                            org_members::updated_at.eq(today),
-                        ))
-                        .execute(conn)?;
+        let org_member_query = r#"
+            INSERT INTO org_members
+            (
+                id,
+                org_id,
+                user_id,
+                roles,
+                status,
+                created_at,
+                updated_at
+            )
+            VALUES
+            (
+                :id,
+                :org_id,
+                :user_id,
+                :roles,
+                :status,
+                :created_at,
+                :updated_at
+            )
+        "#;
 
-                    // Create superuser entry
-                    let _ = diesel::insert_into(superusers::table)
-                        .values((superusers::id.eq(user_id), superusers::created_at.eq(today)))
-                        .execute(conn)?;
+        let mut org_member_params = new_query_params();
+        org_member_params.push(text_param(":id", org_member_id));
+        org_member_params.push(text_param(":org_id", org_id));
+        org_member_params.push(text_param(":user_id", user_id.clone()));
+        org_member_params.push(text_param(":roles", "Superuser".to_string()));
+        org_member_params.push(text_param(":status", "active".to_string()));
+        org_member_params.push(integer_param(":created_at", created_at));
+        org_member_params.push(integer_param(":updated_at", created_at));
 
-                    Ok(SuperuserDto {
-                        id: user_id,
-                        created_at: today.to_rfc3339_opts(SecondsFormat::Millis, true),
-                    })
-                })
-            })
+        let superuser_query = r#"
+            INSERT INTO superusers
+            (
+                id,
+                created_at
+            )
+            VALUES
+            (
+                :id,
+                :created_at
+            )
+        "#;
+
+        let mut superuser_params = new_query_params();
+        superuser_params.push(text_param(":id", user_id.clone()));
+        superuser_params.push(integer_param(":created_at", created_at));
+
+        let tx = self
+            .db_pool
+            .transaction()
             .await
-            .context(DbInteractSnafu)?;
+            .context(DbTransactionSnafu)?;
 
-        let superuser = trans_res.context(DbQuerySnafu {
-            table: "superusers".to_string(),
-        })?;
+        let mut user_stmt = tx.prepare(user_query).await.context(DbPrepareSnafu)?;
+        let user_affected = user_stmt
+            .execute(user_params)
+            .await
+            .context(DbStatementSnafu)?;
+        assert!(user_affected > 0, "Must insert a new user row");
 
-        Ok(superuser)
+        let mut password_stmt = tx.prepare(passwd_query).await.context(DbPrepareSnafu)?;
+        let password_affected = password_stmt
+            .execute(password_params)
+            .await
+            .context(DbStatementSnafu)?;
+        assert!(password_affected > 0, "Must insert a new password row");
+
+        let mut org_stmt = tx.prepare(org_query).await.context(DbPrepareSnafu)?;
+        let org_affected = org_stmt
+            .execute(org_params)
+            .await
+            .context(DbStatementSnafu)?;
+        assert!(org_affected > 0, "Must insert a new org row");
+
+        let mut org_member_stmt = tx.prepare(org_member_query).await.context(DbPrepareSnafu)?;
+        let org_member_affected = org_member_stmt
+            .execute(org_member_params)
+            .await
+            .context(DbStatementSnafu)?;
+        assert!(org_member_affected > 0, "Must insert a new org member row");
+
+        let mut superuser_stmt = tx.prepare(superuser_query).await.context(DbPrepareSnafu)?;
+        let superuser_affected = superuser_stmt
+            .execute(superuser_params)
+            .await
+            .context(DbStatementSnafu)?;
+        assert!(superuser_affected > 0, "Must insert a new superuser row");
+
+        tx.commit().await.context(DbTransactionSnafu)?;
+
+        Ok(SuperuserDto {
+            id: user_id,
+            created_at,
+        })
     }
 
     pub async fn list(&self) -> Result<Vec<SuperuserDto>> {
-        let db = self.db_pool.get().await.context(DbPoolSnafu)?;
+        let query = r#"
+            SELECT
+                id,
+                created_at
+            FROM superusers
+            ORDER BY created_at ASC
+        "#;
 
-        let select_res = db
-            .interact(move |conn| {
-                dsl::superusers
-                    .select(Superuser::as_select())
-                    .load::<Superuser>(conn)
-            })
-            .await
-            .context(DbInteractSnafu)?;
+        let q_params = new_query_params();
 
-        let items = select_res.context(DbQuerySnafu {
-            table: "superusers".to_string(),
-        })?;
-
-        let items: Vec<SuperuserDto> = items.into_iter().map(|x| x.into()).collect();
+        let mut stmt = self.db_pool.prepare(query).await.context(DbPrepareSnafu)?;
+        let mut rows = stmt.query(q_params).await.context(DbStatementSnafu)?;
+        let items: Vec<SuperuserDto> = collect_rows(&mut rows).await?;
 
         Ok(items)
     }
 
-    pub async fn create(&self, user_id: i32) -> Result<SuperuserDto> {
-        let db = self.db_pool.get().await.context(DbPoolSnafu)?;
+    pub async fn create(&mut self, user_id: String) -> Result<SuperuserDto> {
+        let query = r#"
+            INSERT INTO superusers
+            (
+                id,
+                created_at
+            )
+            VALUES
+            (
+                :id,
+                :created_at
+            )
+        "#;
 
-        let doc = Superuser {
+        let created_at = chrono::Utc::now().timestamp_millis();
+
+        let mut q_params = new_query_params();
+        q_params.push(text_param(":id", user_id.clone()));
+        q_params.push(integer_param(":created_at", created_at));
+
+        let mut stmt = self.db_pool.prepare(query).await.context(DbPrepareSnafu)?;
+        let affected = stmt.execute(q_params).await.context(DbStatementSnafu)?;
+        assert!(affected > 0, "Must insert a new superuser row");
+
+        Ok(SuperuserDto {
             id: user_id,
-            created_at: Utc::now(),
-        };
-
-        let doc_copy = doc.clone();
-
-        let inser_res = db
-            .interact(move |conn| {
-                diesel::insert_into(superusers::table)
-                    .values(&doc_copy)
-                    .execute(conn)
-            })
-            .await
-            .context(DbInteractSnafu)?;
-
-        let _ = inser_res.context(DbQuerySnafu {
-            table: "superusers".to_string(),
-        })?;
-
-        Ok(doc.into())
+            created_at,
+        })
     }
 
-    pub async fn get(&self, id: i32) -> Result<Option<SuperuserDto>> {
-        let db = self.db_pool.get().await.context(DbPoolSnafu)?;
+    pub async fn get(&self, id: String) -> Result<Option<SuperuserDto>> {
+        let query = r#"
+            SELECT
+                id,
+                created_at
+            FROM superusers
+            WHERE
+                id = :id
+            LIMIT 1
+        "#;
 
-        let select_res = db
-            .interact(move |conn| {
-                dsl::superusers
-                    .find(id)
-                    .select(Superuser::as_select())
-                    .first::<Superuser>(conn)
-                    .optional()
-            })
-            .await
-            .context(DbInteractSnafu)?;
+        let mut q_params = new_query_params();
+        q_params.push(text_param(":id", id));
 
-        let user = select_res.context(DbQuerySnafu {
-            table: "superusers".to_string(),
-        })?;
-
-        Ok(user.map(|x| x.into()))
+        let mut stmt = self.db_pool.prepare(query).await.context(DbPrepareSnafu)?;
+        let row_result = stmt.query_row(q_params).await;
+        let dto: Option<SuperuserDto> = collect_row(row_result)?;
+        Ok(dto)
     }
 }
