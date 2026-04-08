b8ae9adb4749c099ca273c2ae25cf14efaba88aa
Leonel <114853+lysender@users.noreply.github.com>
2026-04-08 10:33:43 +0800

    fix: enable http logger (#29)
    Co-authored-by: Lysender <lysender@darkstar>

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ mod utils;
 mod validators;
 mod web;
 
-use std::process;
-use tracing::info;
+use std::{process, str::FromStr};
+use tracing::{Level, info};
 
 use config::Config;
 
@@ -24,7 +24,16 @@ use crate::run::run;
 
 #[tokio::main]
 async fn main() {
+    let mut max_log = Level::INFO;
+    if let Some(rust_log_max) = std::env::var_os("RUST_LOG_MAX") {
+        max_log = Level::from_str(rust_log_max.to_str().unwrap()).unwrap_or_else(|e| {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        });
+    }
+
     tracing_subscriber::fmt()
+        .with_max_level(max_log)
         .with_target(false)
         .compact()
         .init();
